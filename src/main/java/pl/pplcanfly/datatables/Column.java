69a6374fe98389009a46f9948ef25e3d06d0f908
pplcanfly <sleep.etc@gmail.com>
2011-11-20 01:09:31 +0100

    class for defining columns

---
+++ src/main/java/pl/pplcanfly/datatables/Column.java
@@ -0,0 +1,32 @@
+package pl.pplcanfly.datatables;
+
+import pl.pplcanfly.datatables.accessors.ValueAccessor;
+
+public class Column {
+    private Type type;
+    private String name;
+    private ValueAccessor valueAccessor;
+
+    public Column(Type type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public Column(Type type, ValueAccessor valueAccessor) {
+        this.type = type;
+        this.valueAccessor = valueAccessor;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ValueAccessor getValueAccessor() {
+        return valueAccessor;
+    }
+
+}
