5a6a44a85dede06adb7b29bf6d32b387734b7b38
pplcanfly <sleep.etc@gmail.com>
2011-11-30 23:43:43 +0100

    performance refactoring

--- src/main/java/pl/pplcanfly/datatables/filtering/DefaultFilter.java
+++ src/main/java/pl/pplcanfly/datatables/filtering/DefaultFilter.java
@@ -2,6 +2,7 @@ package pl.pplcanfly.datatables.filtering;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import pl.pplcanfly.datatables.Column;
 import pl.pplcanfly.datatables.ServerSideDataTable;
@@ -20,26 +21,44 @@ public class DefaultFilter implements Filter {
     @Override
     public List<?> filter(List<?> rows) {
         List<Object> filteredRows = new ArrayList<Object>();
-        String search = params.getSearch();
-        List<String> searchableCols = params.getSearchableCols();
+        List<Pattern> patterns = precompile();
+        List<Column> columns = findColumns();
+
+    outer:
         for (Object row : rows) {
-            StringBuilder sb = new StringBuilder();
-            for (String columnName : searchableCols) {
-                Column column = dataTable.findColumn(columnName);
-                sb.append(column.getValueAccessor().getValueFrom(row)).append(" ");
-            }
-            String[] splitted = search.split(" ");
-            boolean matches = false;
-            for (String s : splitted) {
-                if (sb.toString().toLowerCase().matches(".*" + s.toLowerCase() + ".*")) {
-                    matches = true;
+            for (Column column : columns) {
+                String columnValue = column.getValueAccessor().getValueFrom(row).toString();
+                for (Pattern pattern : patterns) {
+                    if (pattern.matcher(columnValue).matches()) {
+                        filteredRows.add(row);
+                        continue outer;
+                    }
                 }
             }
-            if (matches) {
-                filteredRows.add(row);
-            }
         }
+
         return filteredRows;
+    }
+
+    private List<Column> findColumns() {
+        List<String> searchableCols = params.getSearchableCols();
+        List<Column> columns = new ArrayList<Column>();
+        for (String columnName : searchableCols) {
+            columns.add(dataTable.findColumn(columnName));
+        }
+        return columns;
+    }
+
+    private List<Pattern> precompile() {
+        String search = params.getSearch();
+
+        List<Pattern> patterns = new ArrayList<Pattern>();
+        String[] splitted = search.split(" ");
+        for (String s : splitted) {
+            Pattern pattern = Pattern.compile(".*" + s.toLowerCase() + ".*", Pattern.CASE_INSENSITIVE);
+            patterns.add(pattern);
+        }
+        return patterns;
     }
 
 }
