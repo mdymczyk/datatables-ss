497acb0db42c9fe8462677f476588a230a75ea57
pplcanfly <sleep.etc@gmail.com>
2011-12-04 13:02:24 +0100

    accessing properties via fields/getters/bool getters

--- src/main/java/pl/pplcanfly/datatables/accessors/ReflectionValueAccessor.java
+++ src/main/java/pl/pplcanfly/datatables/accessors/ReflectionValueAccessor.java
@@ -1,12 +1,16 @@
 package pl.pplcanfly.datatables.accessors;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class ReflectionValueAccessor implements ValueAccessor {
 
     private String fieldName;
     private Field field;
+    private Method method;
 
     public ReflectionValueAccessor(String fieldName) {
         this.fieldName = fieldName;
@@ -14,15 +18,59 @@ public class ReflectionValueAccessor implements ValueAccessor {
 
     @Override
     public Object getValueFrom(Object obj) {
+        if (field == null && method == null) {
+            initialize(obj);
+        }
+
         try {
-            if (field == null) {
-                field = obj.getClass().getDeclaredField(fieldName);
-                field.setAccessible(true);
-            }
-            return field.get(obj);
+            return field != null ? field.get(obj) : method.invoke(obj, (Object[])null);
         } catch (Exception e) {
             throw new ValueNotAccessibleException(e);
         }
+    }
+
+    private void initialize(Object obj) {
+        Class<? extends Object> klass = obj.getClass();
+
+        field = findField(klass.getDeclaredFields());
+        if (field == null) {
+            field = findField(klass.getFields());
+        }
+        if (field != null) {
+            field.setAccessible(true);
+        } else {
+            method = findMethod(klass.getDeclaredMethods());
+            if (method == null) {
+                method = findMethod(klass.getMethods());
+            }
+        }
+    }
+
+    private Field findField(Field[] fields) {
+        for (Field f : fields) {
+            if (f.getName().equals(fieldName)) {
+                return f;
+            }
+        }
+        return null;
+    }
+
+    private Method findMethod(Method[] methods) {
+        List<String> getters = Arrays.asList(getter(), boolGetter());
+        for (Method m : methods) {
+            if (getters.contains(m.getName())) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    private String getter() {
+        return "get" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
+    }
+
+    private String boolGetter() {
+        return "is" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
     }
 
 }
--- src/test/java/pl/pplcanfly/datatables/accessors/ReflectionValueAccessorTest.java
+++ src/test/java/pl/pplcanfly/datatables/accessors/ReflectionValueAccessorTest.java
@@ -11,9 +11,23 @@ import pl.pplcanfly.datatables.accessors.ValueNotAccessibleException;
 public class ReflectionValueAccessorTest {
 
     @Test
-    public void should_get_value_from_object() {
+    public void should_get_value_from_object_by_declared_field() {
         // given
-        Something s = new Something("name", 22);
+        RefSomething s = new RefSomething();
+
+        ReflectionValueAccessor accessor = new ReflectionValueAccessor("field");
+
+        // when
+        Object value = accessor.getValueFrom(s);
+
+        // then
+        assertThat(value).isEqualTo("fieldVal");
+    }
+
+    @Test
+    public void should_get_value_from_object_by_declared_getter_method() {
+        // given
+        RefSomething s = new RefSomething();
 
         ReflectionValueAccessor accessor = new ReflectionValueAccessor("foo");
 
@@ -21,7 +35,63 @@ public class ReflectionValueAccessorTest {
         Object value = accessor.getValueFrom(s);
 
         // then
-        assertThat(value).isEqualTo("name");
+        assertThat(value).isEqualTo("getFoo");
+    }
+
+    @Test
+    public void should_get_value_from_object_by_inherited_method() {
+        // given
+        RefSomethingSub s = new RefSomethingSub();
+
+        ReflectionValueAccessor accessor = new ReflectionValueAccessor("foo");
+
+        // when
+        Object value = accessor.getValueFrom(s);
+
+        // then
+        assertThat(value).isEqualTo("getFoo");
+    }
+
+    @Test
+    public void should_get_value_from_object_by_inherited_field() {
+        // given
+        RefSomethingSub s = new RefSomethingSub();
+
+        ReflectionValueAccessor accessor = new ReflectionValueAccessor("publicField");
+
+        // when
+        Object value = accessor.getValueFrom(s);
+
+        // then
+        assertThat(value).isEqualTo("publicFieldVal");
+    }
+
+    @Test
+    public void should_get_value_from_object_by_inherited_overriden_method() {
+        // given
+        RefSomethingSub s = new RefSomethingSub();
+
+        ReflectionValueAccessor accessor = new ReflectionValueAccessor("bar");
+
+        // when
+        Object value = accessor.getValueFrom(s);
+
+        // then
+        assertThat(value).isEqualTo("getBarSub");
+    }
+
+    @Test
+    public void should_get_value_from_object_by_getter_bool_method() {
+        // given
+        RefSomethingSub s = new RefSomethingSub();
+
+        ReflectionValueAccessor accessor = new ReflectionValueAccessor("baz");
+
+        // when
+        Object value = accessor.getValueFrom(s);
+
+        // then
+        assertThat(value).isEqualTo("isBaz");
     }
 
     @Test(expected=ValueNotAccessibleException.class)
@@ -37,4 +107,32 @@ public class ReflectionValueAccessorTest {
         // then throw exception
     }
 
+}
+
+class RefSomething {
+    @SuppressWarnings("unused")
+    private String field = "fieldVal";
+    public String publicField = "publicFieldVal";
+
+    public String getFoo() {
+        return "getFoo";
+    }
+
+    public String getBar() {
+        return "getBar";
+    }
+}
+
+class RefSomethingSub extends RefSomething {
+
+    // private foo field is not accessible here directly, only via getter
+
+    @Override
+    public String getBar() {
+        return "getBarSub";
+    }
+
+    public String isBaz() {
+        return "isBaz";
+    }
 }
