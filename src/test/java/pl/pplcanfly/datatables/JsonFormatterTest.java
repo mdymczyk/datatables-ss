c517067355a11c5cd539d8ffa17969a689a4a7a3
pplcanfly <sleep.etc@gmail.com>
2011-12-13 20:46:34 +0100

    test moved

--- src/test/java/pl/pplcanfly/datatables/JsonFormatterTest.java
+++ src/test/java/pl/pplcanfly/datatables/JsonFormatterTest.java
@@ -7,32 +7,32 @@ import java.util.List;
 
 import org.junit.Test;
 
-import pl.pplcanfly.datatables.DataTablesResponse;
-import pl.pplcanfly.datatables.ServerSideDataTable;
 import pl.pplcanfly.datatables.params.ResponseParams;
 import pl.pplcanfly.datatables.types.Types;
 
-public class DataTablesResponseTest {
+public class JsonFormatterTest {
 
     @Test
     public void should_transform_to_json() {
         // given
+        ResponseParams responseParams = new ResponseParams(3, 20, 2);
+
         ServerSideDataTable dataTable = ServerSideDataTable.build()
                 .column(Types.text(), "foo")
                 .column(Types.numeric(), "bar")
                 .done();
 
         List<Something> rows = Arrays.asList(new Something("abc", 123), new Something("def", 987));
-        DataTablesResponse response = new DataTablesResponse(new ResponseParams(3, 20, 2), dataTable, rows);
 
         // when
-        String json = response.toJson();
+        String json = new JsonFormatter().format(responseParams, dataTable, rows);
 
         // then
         assertThat(json).isEqualTo("{\"sEcho\":3," +
-        		"\"iTotalRecords\":20," +
-        		"\"iTotalDisplayRecords\":2," +
-        		"\"aaData\":[[\"abc\",123],[\"def\",987]]}");
+                "\"iTotalRecords\":20," +
+                "\"iTotalDisplayRecords\":2," +
+                "\"aaData\":[[\"abc\",123],[\"def\",987]]}");
     }
+
 
 }
